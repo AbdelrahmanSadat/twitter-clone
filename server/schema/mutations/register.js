afe985bfa0675f735eeee172ef7d09d3aaa74d3b
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2019-01-05 08:01:32 +0200

    Added email and password validation.

--- server/schema/mutations/register.js
+++ server/schema/mutations/register.js
@@ -1,6 +1,7 @@
 const graphql = require("graphql");
 const admin = require("firebase-admin");
 const helpers = require("../../helpers");
+const validator = require("validator");
 const User = require("../../models/user.js");
 const UserType = require("../types/UserType");
 
@@ -17,6 +18,10 @@ const register = {
     password: { type: new GraphQLNonNull(GraphQLString)}
   },
   async resolve(parent, args, req){
+    if(!validator.isEmail(args.email))
+      throw new Error("Please provide a valid email address");
+    if(args.password.length<8)
+      throw new Error("Password should be at least 8 characters");
     const user = await User.create({
       email: args.email,
       password: args.password,
