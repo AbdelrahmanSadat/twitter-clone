5c86252e556dc77cfde13623e16641665c7319f5
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2019-01-10 15:19:24 +0200

    Minor changes

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'uploads/images')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
